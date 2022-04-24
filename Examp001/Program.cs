bf6aa385f0061507f5a1b7e3d961e368481953c5
Abylai <aba.omar@mail.ru>
2022-04-25 00:20:51 +0600

    add Examp001

---
+++ Examp001/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//using static System.Console;
+//Console.Clear;
+
+//int a;
+Console.Write("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string p = a.ToString();
+char[] ar = p.ToCharArray();
+Array.Reverse(ar);
+p = new string(ar);
+string b = a.ToString();
+Console.WriteLine(a < 10000 || a > 99999? "число не соответствует": b == p? "палиндром" : "не палиндром");
